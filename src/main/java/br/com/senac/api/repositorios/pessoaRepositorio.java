474cd2973c225f7dc39b6002684ffd081238d799
Khalifa Y <riqueanjo666@gmail.com>
2026-03-28 11:15:10 -0300

    Atividade animais feita

---
+++ src/main/java/br/com/senac/api/repositorios/pessoaRepositorio.java
@@ -0,0 +1,9 @@
+package br.com.senac.api.repositorios;
+
+import br.com.senac.api.entidades.Pessoas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface pessoaRepositorio extends JpaRepository<Pessoas, Long> {
+}
